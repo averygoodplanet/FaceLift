f92d79e61b95ded6e298cb69c23c23a5b9766d28
averygoodplanet <averygoodplanet@gmail.com>
2014-03-20 17:18:09 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FaceLift::Application.config.secret_key_base = '65fdde3a2348c61a9a08e02eb11f7461df7ccb2eb8cb25ff604fa5b11e634bc05a60fb7fa01c08003c4c9a04ccd4547f82645ddf5dc734352737f4cd854cb256'
