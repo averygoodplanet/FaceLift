b17fac198667051966708843356863d64fe94e54
averygoodplanet <averygoodplanet@gmail.com>
2014-03-21 14:52:02 -0500

    successfully upload and display both before and after images (not styled)

--- app/models/website.rb
+++ app/models/website.rb
@@ -1,6 +1,7 @@
 class Website < ActiveRecord::Base
   attr_accessible :title, :description, :before_image, :after_image, :image_cache
   mount_uploader :before_image, ImageUploader
+  mount_uploader :after_image, ImageUploader
 
   validates_presence_of :title
 end
