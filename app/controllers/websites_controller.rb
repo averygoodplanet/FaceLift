ca3476dbda86e5669e02232ea0f8e33a9dea7fe5
averygoodplanet <averygoodplanet@gmail.com>
2014-03-27 18:24:32 -0500

    order website examples by alphabetical order

--- app/controllers/websites_controller.rb
+++ app/controllers/websites_controller.rb
@@ -1,28 +1,14 @@
 class WebsitesController < ApplicationController
   before_action :set_website, only: [:show, :edit, :update, :destroy]
 
-  # GET /websites
-  # GET /websites.json
   def index
-    @websites = Website.all
+    @websites = Website.order(title: :asc)
   end
 
-  # GET /websites/1
-  # GET /websites/1.json
-  def show
-  end
-
-  # GET /websites/new
   def new
     @website = Website.new
   end
 
-  # GET /websites/1/edit
-  def edit
-  end
-
-  # POST /websites
-  # POST /websites.json
   def create
     return unless current_user
 
@@ -38,22 +24,6 @@ class WebsitesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /websites/1
-  # PATCH/PUT /websites/1.json
-  def update
-    respond_to do |format|
-      if @website.update(website_params)
-        format.html { redirect_to @website, notice: 'Website was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @website.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /websites/1
-  # DELETE /websites/1.json
   def destroy
     return unless current_user
 
