7726d7c98b405d9684c3c18dee39c417a7440104
averygoodplanet <averygoodplanet@gmail.com>
2014-03-27 17:02:27 -0500

    seeded database with admin user, able to login and logout

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  attr_accessible :email, :password, :password_confirmation
   # Removed :registerable from defaults, because we want only one user (don't want new users registering.)
   devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# user = User.new :name => 'Gary Busey', :email => 'averygoodplanet@gmail.com', :password => 'topSecret9*', :password_confirmation => 'topSecret9*'
+# user.save!
+
+# user = User.new(
+#     :email                 => "admin@xxxxx.xxx",
+#     :password              => "123456",
+#     :password_confirmation => "123456"
+# )
+# # user.skip_confirmation!
+# user.save!
+
+User.create!(
+  :email => 'averygoodplanet@gmail.com',
+  :first_login => false,
+  :password => 'P@ssw0rd',
+  :password_confirmation => 'P@ssw0rd'
+  )
